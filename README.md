25a0e2582d6d8b14739cdb58d7f6b324cd84e6e4
wanglf19 <wanglf@big.ac.cn>
2022-07-25 20:09:53 +0800

    add

---
+++ README.md
@@ -0,0 +1,96 @@
+## A deep generative framework with embedded vector arithmetic and classifier for sample generation, label transfer, and clustering of transcriptome data
+
+This repository contains the official Keras implementation of:
+
+**A deep generative framework with embedded vector arithmetic and classifier for sample generation, label transfer, and clustering of transcriptome data**
+
+**Requirements**
+- Python 3.6
+- conda 4.4.10
+- keras 2.2.4
+- tensorflow 1.11.0
+
+**1. Model training**
+
+- *About this article*
+```
+#Augments:
+#'--inputdata', type=str, default='data/training_data/Fig2_scgen_count7000r.npz', help='address for input data')
+#'--input_covariates', type=str, default='data/training_data/Fig2_scgen_study_condition.npy', help='address for covariate (e.g. batch)')
+#'--inputcelltype', type=str, default='data/training_data/Fig2_scgen_cell_type.npy', help='address for celltype label')
+#'--randoms', type=int, default=30, help='random number to split dataset')
+#'--permute_input', type=str, default='T', help='whether permute the input')
+
+#'--dim_latent', type=int, default=50, help='dimension of latent space')
+#'--dim_intermediate', type=int, default=200, help='dimension of intermediate layer')
+#'--activation', type=str, default='relu', help='activation function: relu or tanh')
+#'--arithmetic', type=str, default='minus', help='arithmetic: minus or plus')
+
+#'--batch_size', type=int, default=500, help='training parameters_batch_size')
+#'--epochs', type=int, default=50, help='training parameters_epochs')
+
+#'--training', type=str, default='T', help='training model(T) or loading model(F) ')
+#'--weights', type=str, default='data/weights_and_results/Fig2_demo.weight', help='trained weights')
+
+#'--mode', type=str, default='supervised', help='mode: supervised, semi_supervised, unsupervised, user_defined')
+
+#'--reconstruction_loss', type=int, default=5, help='The reconstruction loss for VAE')
+#'--kl_cross_loss', type=int, default=1, help='')
+#'--prior_distribution_loss', type=int, default=0, help='The assumption that prior distribution is uniform distribution')
+#'--label_cross_loss', type=int, default=20, help='Loss for integrating label information into the model')
+
+
+For supervised mode
+python inClust.py --inputdata=data/training_data/Fig2_scgen_count7000r.npz --input_covariates=data/training_data/Fig2_scgen_study_condition.npy --inputcelltype=data/training_data/Fig2_scgen_cell_type.npy --mode=supervised
+
+For semi_supervised mode
+python inClust.py --inputdata=data/training_data/Fig3_heart_count.npz --input_covariates=data/training_data/Fig3_heart_batch.npy --inputcelltype=data/training_data/Fig3_heart_label_semi.npy --mode=semi_supervised --permute_input=F
+
+For unsupervised mode
+python inClust.py --inputdata=data/training_data/Fig4_count_pca_log.npy --input_covariates=data/training_data/Fig4_img_rgb_smooth50.npy --inputcelltype=data/training_data/Fig4_label.npy --mode=unsupervised  --arithmetic=plus
+
+```
+
+- *Further Explore*
+```
+#- Testing your own dataset
+python inClust.py --inputdata=your_data --input_covariates=your_inputcelltype --inputcelltype=your_inputcelltype --mode=your_mode
+
+- *training weight*
+results/training.weight
+
+
+**2. Analysis**
+
+- *Demo -- About this article*
+
+The following codes could generate analysis data in the main text.
+```
+For supervised mode
+python inClust.py --inputdata=data/training_data/Fig2_scgen_count7000r.npz --input_covariates=data/training_data/Fig2_scgen_study_condition.npy --inputcelltype=data/training_data/Fig2_scgen_cell_type.npy --mode=supervised --training=F --weights=data/weights_and_results/Fig2_demo.weight
+
+For semi_supervised mode
+python inClust.py --inputdata=data/training_data/Fig3_heart_count.npz --input_covariates=data/training_data/Fig3_heart_batch.npy --inputcelltype=data/training_data/Fig3_heart_label_semi.npy --mode=semi_supervised --training=F --weights=data/weights_and_results/Fig3_demo.weight
+
+For unsupervised mode
+python inClust.py --inputdata=data/training_data/Fig4_count_pca_log.npy --input_covariates=data/training_data/Fig4_img_rgb_smooth50.npy --inputcelltype=data/training_data/Fig4_label.npy --mode=unsupervised  --arithmetic=plus --training=F --weights=data/weights_and_results/Fig4_demo.weight
+
+```
+
+- *Output*
+The output is in the results folder, including
+- *two latent space representation*
+```
+mean_vector.npy
+batch_vector.npy
+```
+
+- *predictd label for each sample*
+predict_labels.csv
+
+**3. reproduction figures**
+demo_reproducing_figures.ipynb
+
+
+**VAE and its variant implementation**
+https://github.com/bojone/vae
---
+++ inClust.py
@@ -0,0 +1,265 @@
+#! -*- coding: utf-8 -*-
+#the VAE and its variants (cVAE and VAE with clustering)could be refer to https://github.com/bojone/vae
+import csv
+from scipy import sparse
+from keras import optimizers
+import numpy as np
+from keras.layers import *
+from keras.models import Model
+from keras import backend as K
+from sklearn.decomposition import PCA
+# import imageio,os
+# from keras.datasets import mnist
+# from keras.datasets import fashion_mnist as mnist
+# from cortex import CortexDataset  #wang
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import seaborn as sns
+from pandas import DataFrame
+import os, csv, re
+import argparse
+
+#'''
+#####################################################################################################################
+parser = argparse.ArgumentParser(description='inClust')
+# system config
+parser.add_argument('--inputdata', type=str, default='data/training_data/Fig2_scgen_count7000r.npz', help='address for input data')
+parser.add_argument('--input_covariates', type=str, default='data/training_data/Fig2_scgen_study_condition.npy', help='address for covariate (e.g. batch)')
+parser.add_argument('--inputcelltype', type=str, default='data/training_data/Fig2_scgen_cell_type.npy', help='address for celltype label')
+parser.add_argument('--permute_input', type=str, default='T', help='whether permute the input')
+parser.add_argument('--randoms', type=int, default=30, help='random number to split dataset')
+
+parser.add_argument('--dim_latent', type=int, default=50, help='dimension of latent space')
+parser.add_argument('--dim_intermediate', type=int, default=200, help='dimension of intermediate layer')
+parser.add_argument('--activation', type=str, default='relu', help='activation function: relu or tanh')
+parser.add_argument('--arithmetic', type=str, default='minus', help='arithmetic: minus or plus')
+
+parser.add_argument('--batch_size', type=int, default=500, help='training parameters_batch_size')
+parser.add_argument('--epochs', type=int, default=50, help='training parameters_epochs')
+
+parser.add_argument('--training', type=str, default='T', help='training model(T) or loading model(F) ')
+parser.add_argument('--weights', type=str, default='data/weights_and_results/Fig2_demo.weight', help='trained weights')
+
+parser.add_argument('--mode', type=str, default='suprevised', help='mode: supervised, semi_supervised, unsupervised, user_defined')
+
+parser.add_argument('--reconstruction_loss', type=int, default=5, help='The reconstruction loss for VAE')
+parser.add_argument('--kl_cross_loss', type=int, default=1, help='')
+parser.add_argument('--prior_distribution_loss', type=int, default=0, help='The assumption that prior distribution is uniform distribution')
+parser.add_argument('--label_cross_loss', type=int, default=20, help='Loss for integrating label information into the model')
+
+
+args = parser.parse_args()
+inputdata = args.inputdata
+inputbatch = args.input_covariates
+inputcelltype = args.inputcelltype
+randoms = args.randoms
+permute_input = args.permute_input
+arithmetic = args.arithmetic
+
+z_dim = args.dim_latent
+intermediate_dim = args.dim_intermediate
+activation_function = args.activation
+
+epochs = args.epochs
+batch_size = args.batch_size
+
+training = args.training
+weights = args.weights
+
+reconstruction_loss = args.reconstruction_loss
+kl_cross_loss = args.kl_cross_loss
+prior_distribution_loss = args.prior_distribution_loss
+label_cross_loss = args.label_cross_loss
+
+mode = args.mode
+if mode == 'supervised':
+    reconstruction_loss = 5
+    kl_cross_loss = 1
+    prior_distribution_loss = 0
+    label_cross_loss = 20
+if mode == 'semi_supervised':
+    reconstruction_loss = 5
+    kl_cross_loss = 1
+    prior_distribution_loss = 0
+    label_cross_loss = 50
+if mode == 'unsupervised':
+    reconstruction_loss = 5
+    kl_cross_loss = 1
+    prior_distribution_loss = 1
+    label_cross_loss = 0
+
+
+
+if inputdata[-3:] == 'npz':
+    data = sparse.load_npz(inputdata)
+    data = data.todense()
+else:
+    data = np.load(inputdata)
+
+batch = np.load(inputbatch)
+labels = np.load(inputcelltype)
+
+print(data.shape)
+print(batch.shape)
+print(labels.shape)
+
+num_batch = batch.shape[1]
+num_clusters = labels.shape[1]
+
+label = np.hstack((batch,labels))
+print(label.shape)
+
+if permute_input == 'T':
+    x_train, x_test, y_train, y_test = train_test_split(data, label, test_size=0, random_state=30)
+else:
+    x_train = data
+    y_train = label
+
+print(x_train.shape)
+print(y_train.shape)
+
+batch_information_data = y_train[:,0:num_batch]
+label_information_data = y_train[:,num_batch:]
+
+print(x_train)
+print(batch_information_data)
+print(label_information_data)
+
+# model
+#encoder
+input_size = x_train.shape[1]
+x_in = Input(shape=(input_size,))
+x = Dense(intermediate_dim, activation=activation_function)(x_in)
+x = Dense(z_dim, activation=activation_function)(x)
+
+z_mean = Dense(z_dim)(x)
+z_log_var = Dense(z_dim)(x)
+
+#embedding layer
+batch_information = Input(shape=(num_batch,))
+if activation_function == 'tanh':
+    yh = Dense(z_dim, activation='tanh')(batch_information)
+else:
+    yh = Dense(z_dim)(batch_information)
+
+label_information = Input(shape=(num_clusters,))
+encoder = Model([x_in, batch_information, label_information], [z_mean, yh])
+
+#decoder
+z1 = Input(shape=(z_dim,))
+h = Dense(intermediate_dim, activation=activation_function)(z1)
+x_recon = Dense(input_size, activation=activation_function)(h)
+
+decoder = Model(z1, x_recon)
+
+#classifier
+z = Input(shape=(z_dim,))
+y = Dense(num_clusters, activation='softmax')(z)
+
+classfier = Model(z, y)
+
+#reparameterization trick
+def sampling(args):
+    z_mean, z_log_var = args
+    epsilon = K.random_normal(shape=(K.shape(z_mean)[0], z_dim))
+    return z_mean + K.exp(z_log_var / 2) * epsilon
+
+z1 = Lambda(sampling, output_shape=(z_dim,))([z_mean, z_log_var])
+x_recon = decoder(z1)
+
+#vector arithmetic
+def arithmetics(args):
+    z1, yh = args
+    if arithmetic == 'minus':
+        return z1 - yh
+    else:
+        return z1 + yh
+
+z = Lambda(arithmetics, output_shape=(z_dim,))([z1, yh])
+y = classfier(z)
+
+#parameter for mean vector of each cluster
+class Gaussian(Layer):
+    def __init__(self, num_clusters, **kwargs):
+        self.num_clusters = num_clusters
+        super(Gaussian, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        latent_dim = input_shape[-1]
+        self.mean = self.add_weight(name='mean',
+                                    shape=(self.num_clusters, latent_dim),
+                                    initializer='zeros')
+
+    def call(self, inputs):
+        z = inputs
+        z = K.expand_dims(z, 1)
+        return z * 0 + K.expand_dims(self.mean, 0)
+
+    def compute_output_shape(self, input_shape):
+        return (None, self.num_clusters, input_shape[-1])
+
+
+gaussian = Gaussian(num_clusters)
+z_prior_mean = gaussian(z)
+
+vae = Model([x_in, batch_information, label_information], [x_recon, z_prior_mean, y])
+
+#Loss
+z_mean = K.expand_dims(z_mean, 1)
+z_log_var = K.expand_dims(z_log_var, 1)
+yh = K.expand_dims(yh, 1)
+
+#hyper-paramter
+lamb = reconstruction_loss
+lamb1 = kl_cross_loss
+lamb2 = prior_distribution_loss
+lamb3 = label_cross_loss
+
+label_loss = K.mean(-label_information * K.log(y + K.epsilon()), 0)
+xent_loss = 0.5 * K.mean((x_in - x_recon) ** 2, 0)
+kl_loss_origin = - 0.5 * (1 + z_log_var - K.square(z_mean - z_prior_mean - yh) - K.exp(z_log_var))
+kl_loss = K.mean(K.batch_dot(K.expand_dims(y, 1), kl_loss_origin),0)
+cat_loss = K.mean(y * K.log(y + K.epsilon()), 0)
+vae_loss = lamb * K.sum(xent_loss) + lamb1 * K.sum(kl_loss) +lamb2*K.sum(cat_loss) + lamb3 * K.sum(label_loss)
+
+print('vae_loss', type(vae_loss))
+
+vae.add_loss(vae_loss)
+vae.compile(optimizer='adam')
+
+vae.summary()
+Training = training
+if Training == 'T':
+    vae.fit([x_train, batch_information_data, label_information_data],
+            shuffle=True,
+            epochs=epochs,
+            batch_size=batch_size)
+
+    vae.save_weights('results/training.weight')
+    exit()
+
+vae.load_weights(weights)
+means = K.eval(gaussian.mean)
+
+x_train_encoded, y = encoder.predict([data, batch, labels])
+print(x_train_encoded.shape)
+print(y.shape)
+if arithmetic == 'minus':
+    predict_x_train_encoded = x_train_encoded - y
+else:
+    predict_x_train_encoded = x_train_encoded + y
+
+np.save('results/mean_vector.npy', x_train_encoded)
+np.save('results/batch_vector.npy', y)
+
+#X_embedded = TSNE(n_components=2,
+#                init='random').fit_transform(predict_x_train_encoded)
+#print(X_embedded.shape)
+#np.save('results/3tsne.npy',X_embedded)
+
+y_train_pred = classfier.predict(predict_x_train_encoded).argmax(axis=1)
+print(y_train_pred.shape)
+np.save('results/predict_labels.npy', y_train_pred)
+
